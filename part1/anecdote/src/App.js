0e46c50c0bda35051051a5e2083f057a10826aa1
Fahd Mohammed <fahdmoh.1@gmail.com>
2022-06-02 12:30:12 +0000

    anecdote assignment completed

---
+++ part1/anecdote/src/App.js
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+const Header =({text})=>{
+	return(
+		<>
+		<h1>{text}</h1>
+		</>
+	)
+}
+const Button =({handleClick, text})=>{
+	return(
+		<>
+			<button onClick ={handleClick}>{text}</button>
+		</>
+	)
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState([0,0,0,0,0,0,0])
+
+  const handleRandomSelection=()=>{
+    const randValue =  Math.floor(Math.random() * anecdotes.length)
+    setSelected(randValue)
+  }
+
+  const handleVotes=()=>{
+	const newVotes = [...votes]
+	newVotes[selected]++
+	setVote(newVotes)
+  }
+
+  const maxV = Math.max(...votes)
+  const votesIndex = votes.indexOf(maxV)
+
+  return (
+    <div>
+      <Header text='Anecdote of the day'/>
+      <p>{anecdotes[selected]}<br/>has {votes[selected]} votes</p><Button handleClick={handleRandomSelection}  text='next anecdote'/>
+	  <Button handleClick={handleVotes}  text='vote'/>
+	  <Header text='Anecdote with most votes'/>
+	  { maxV !== 0 ? <p>{anecdotes[votesIndex]}<br/> has {maxV}</p> : <p>There are no votes</p> }
+
+    </div>
+  )
+}
+
+export default App
