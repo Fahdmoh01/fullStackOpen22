ab41ffeb7a1223e651d25fae25d23f0570e25d05
Fahd Mohammed <fahdmoh.1@gmail.com>
2022-06-17 21:28:14 +0000

    installed json-server and used axios to fetch data

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,10 +1,27 @@
-import{useState} from 'react'
+import{useState, useEffect} from 'react'
+import axios from 'axios'
+
 import Note from './components/Note'
 
-const App = (props) => {
-  const[notes, setNotes] = useState(props.notes)
+const App = () => {
+  const[notes, setNotes] = useState([])
   const[newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+	const hook = () =>{
+		console.log('effect')
+		axios
+		.get('http://localhost:3001/notes')
+		.then(response => {
+			console.log('promise fulfilled')
+			setNotes(response.data)
+		})
+	}
+
+	useEffect(hook,[]) // the second parameter of the useEffect Method is used to specify how often the effect is run. 
+	//if the second parameter is [] then the effect is only run along witht the first render of the component.
+
+	console.log('render', notes.length, 'notes')
 
 	const addNote = (event) =>{
 		event.preventDefault()
